a724a690561bff6ba9895a7064f3faa5356c76c9
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2015-08-08 15:56:44 +0200

    run sequence of commands

--- src/io/remote.clj
+++ src/io/remote.clj
@@ -3,15 +3,18 @@
             [clj-http.client :as http]
             [clojure.java.io :as io]))
 
-(defn- with-index [vs]
-  (map-indexed vector vs))
+(set! *print-length* 10)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; webservice ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def token "C/1d7grJDHHZEBserpvNAckXRf2+LGVtibuDE8ukD4k=")
 (defn load-problems []
   (map (comp json/read-json slurp) (filter #(.isFile %) (file-seq (io/as-file "resources")))))
 
 (defn parse [url]
   (json/read-json (:body (http/get url))))
+;;;;;;;;;;;;;;;;;;;;;;;; stuff ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn- with-index [vs]
+  (map-indexed vector vs))
 
 (defn random-gen [seed]
   (let [last (atom seed)
@@ -21,9 +24,18 @@
         (swap! last (fn [v] (mod (+ 12345 (* v 1103515245)) (bit-shift-left 1 32))))
         res))))
 
+
+(defn- plus [{x1 :x y1 :y} {x2 :x y2 :y}]
+  {:x (+ x1 x2)
+   :y (+ y1 y2)})
+
+(defn- minus [{x1 :x y1 :y} {x2 :x y2 :y}]
+  {:x (+ x1 x2)
+   :y (+ y1 y2)})
+
 ;;;;;;;;;;;;;;;; board;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn board [{:keys [width height filled]}]
+(defn create-board [{:keys [width height filled]}]
   (mapv vec
         (for [row (range height)]
          (for [col (range width)]
@@ -41,29 +53,28 @@
   (println))
 
 ;;;;;;;;;;;;;;;; units ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defrecord Unit [coords pivot offsets])
-
-(defn create-unit [{:keys [members pivot]}]
-  (let [[min-x max-x] (apply (juxt min max) (map :x members))
-        [min-y max-y] (apply (juxt min max) (map :y members))
-        unit (for [y (range min-y (inc max-y))]
-               (for [x (range min-x (inc max-x))
-                     :let [coord {:x x :y y}]]
-                 (if (some #(= % coord) members)
-                   1
-                   0)))]    
-    (->Unit (mapv vec unit) [(:y pivot) (:x pivot)] [min-y min-x])))
+(defn- unit->matrix [{:keys [members]}]
+  (let [max-x (apply max (map :x members))
+        max-y (apply max (map :y members))]    
+    (mapv vec
+          (for [y (range (inc max-y))]
+            (for [x (range (inc max-x))
+                  :let [coord {:x x :y y}]]
+              (if (some #(= % coord) members)
+                1
+                0))))))
 
 
-(defn print-unit [{:keys [coords pivot]}] 
-  (doseq [[i row] (with-index coords) 
-          :let [odd? (= 1 (mod i 2))]]
-    (when odd? (print "  ")) ; two space because my font seems to be proportional :(
-    (doseq [[j cell] (with-index row)
-            :let [pivot? (= pivot [i j])
-                  chars (if pivot? [\u25ef \u25cf] [\u2B21 \u2B22])]]
-      (print (chars cell) ""))
-    (println)))
+(defn print-unit [{:keys [pivot] :as unit}] 
+  (let [coords (unit->matrix unit)] 
+    (doseq [[i row] (with-index coords) 
+            :let [odd? (= 1 (mod i 2))]]
+      (when odd? (print "  ")) ; two space because my font seems to be proportional :(
+      (doseq [[j cell] (with-index row)
+              :let [pivot? (= pivot {:x j :y i})
+                    chars (if pivot? [\u25ef \u25cf] [\u2B21 \u2B22])]]
+        (print (chars cell) ""))
+      (println))))
 
 (defrecord State [board unit])
 
@@ -71,30 +82,30 @@
   "Convert local coordinates into board coordinates
 so the unit is centered with its top on the top row of the board.
 Returns shifted unit"
-  [board {:keys [coords pivot] :as unit}]
-  (let [board-width (count (first board))
+  [task {:keys [members pivot] :as unit}]
+  (let [board-width (:width task)
+        coords (unit->matrix unit)
         unit-top-row (first coords)
         unit-width (count unit-top-row)
         offset-left (count (take-while zero? unit-top-row))
         offset-right (count (take-while zero? (reverse unit-top-row)))
         unit-top-width (- unit-width offset-left offset-right)
-        offset (- (int (/ (- board-width unit-top-width) 2)) offset-left)]
-    (assoc unit :offsets [0 offset])))
+        offset {:y 0 :x (- (int (/ (- board-width unit-top-width) 2)) offset-left)}]
+    {:pivot (plus pivot offset)
+     :members (mapv (partial plus offset) members)}))
 
-(defn add-to-board [board {coords :coords [off-row off-col] :offsets}]
-  (reduce (fn [board [row-idx unit-row]]
-            (let [unit-row (concat (repeat off-col 0) unit-row)]
-              (update board (+ row-idx off-row) #(into (mapv + unit-row %)
-                                                       (drop (count unit-row) %))))) 
-          board (with-index coords)))
+(defn add-to-board [board {ms :members}]
+  (reduce (fn [board {col :x row :y}]
+            (update-in board [row col] inc)) 
+          board ms))
 
 (defn overlaps-anything? 
   "If the unit would get added to the board, would any cells overlap?"
   [board unit]
-  (every? #{0 1} (apply concat (add-to-board board unit))))
+  (some #{2} (apply concat (add-to-board board unit))))
 
 ;;;;;;;;;;;;;;; coordinates ;;;;;;;;;;;;;;;;;;
-(defn grid->cube [[col row]]
+(defn grid->cube [{col :x row :y}]
   (let [x (- col (/ (- row (bit-and row 1)) 2))
         z row
         y (- (- x) z)] 
@@ -103,12 +114,12 @@ Returns shifted unit"
 (defn cube->grid-even-row [[x y z]]
   (let [col (+ x (/ (- z (bit-and z 1)) 2))
         row z] 
-    [col row]))
+    {:x col :y row}))
 
 (defn cube->grid-odd-row [[x y z]]
   (let [col (+ x (/ (+ z (bit-and z 1)) 2))
         row z]
-    [col row]))
+    {:x col :y row}))
 
 (defn cube-rotate-cw [[x y z]]
   [(- z) 
@@ -121,10 +132,37 @@ Returns shifted unit"
    (- x)])
 
 (defn rotate-around [pivot cell rotator]
-  (let [pc (grid->cube (reverse pivot))
-        cc (grid->cube (reverse cell))]
-    (reverse (cube->grid-even-row (map + pc (rotator (map - cc pc)))))))
+  (let [pc (grid->cube pivot)
+        cc (grid->cube cell)]
+    (cube->grid-even-row (map + pc (rotator (map - cc pc))))))
 
+
+;;;;;;;;;;;;;; movements of units ;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- add-offset [{:keys [members pivot] :as unit} offset]
+  (-> unit
+    (update :pivot plus offset)
+    (update :members (partial mapv (partial plus offset)))))
+
+(defn east [unit]
+  (add-offset unit {:x 1 :y 0}))
+
+(defn west [unit]
+  (add-offset unit {:x -1 :y 0}))
+
+(defn south-east [unit]
+  (add-offset unit {:x 1 :y 1}))
+
+(defn south-west [unit]
+  (add-offset unit {:x -1 :y 1}))
+
+(defn rotate-cw [{:keys [members pivot] :as unit}]
+  (let [pivot-cube (grid->cube pivot)]
+    (assoc unit :members (for [cell members] (rotate-around pivot cell cube-rotate-cw)))))
+
+(defn rotate-ccw [{:keys [members pivot] :as unit}]
+  (let [pivot-cube (grid->cube pivot)]
+    (assoc unit :members (for [cell members] (rotate-around pivot cell cube-rotate-ccw)))))
 
 ;;;;;;;;;;;;;;; commands;;;;;;;;;;;;;;;;;;;;;;
 
@@ -135,32 +173,56 @@ Returns shifted unit"
 (def CW (vec "dqrvz1"))
 (def CCW (vec "kstuwx"))
 
-(defn east [unit]
-  (update-in unit [:offsets 1] inc))
 
-(defn west [unit]
-  (update-in unit [:offsets 1] dec))
+;;;;;;;;;;;;;;;;;; simulator ;;;;;;;;;;;;;;;;;;;;;;;;
+(defn locking? [board unit]
+  (overlaps-anything? board unit))
 
-(defn south-east [unit]
-  (update-in (east unit) [:offsets 0] inc))
+(defn out-of-bounds? 
+  "Does this unit fit within the boundary of the board?"
+  [board unit]
+  (not (and (every? #(< -1 (:x %) (count (first board))) (:members unit))
+           (every? #(< -1 (:y %)) (:members unit)))))
 
-(defn south-west [unit]
-  (update-in (west unit) [:offsets 0] inc))
+(defn bottom-row-reached? [board unit]
+  (let [height (count board)]
+    (some #(= height (:y %)) (:members unit))))
 
-(defn- matrix-to-members [coords]
-  (for [[i row] (with-index coords)
-        [j col] (with-index row)
-        :when (= 1 col)]
-    {:x j :y i}))
+(defn create-source 
+  "Create units in order of seeds using the deterministic random generator"
+  [task seed]
+  (->> seed
+    random-gen
+    (repeatedly (:sourceLength task))
+    (map #(mod % (count (:units task))))
+    (map #(nth (:units task) %))))
 
-(defn rotate-cw [{:keys [coords pivot offsets]}]
-  (let [pivot-cube (grid->cube (map + pivot offsets))
-        members (for [{:keys [x y]} (matrix-to-members coords)
-                      :let [[x y] (rotate-around pivot [y x] cube-rotate-cw)]] 
-                  {:x x :y y})
-        [p-y p-x] pivot]
-    (update (create-unit {:members members :pivot {:x p-x :y p-y}})
-            :offsets #(mapv + offsets %))))
+(defn run-one-command [{:keys [board unit source task] :as state} cmd]
+  (let [unit' (cmd unit)]
+    (cond
+      (out-of-bounds? board unit') (reduced :out-of-bounds)
+      (or (bottom-row-reached? board unit')
+          (locking? board unit')) (let [new-unit (spawn-unit task (first source))] 
+                                    (println "locked!")
+                                    (if (locking? board new-unit)
+                                      (reduced :board-full)
+                                      {:board (add-to-board board unit)
+                                       :unit new-unit
+                                       :task task
+                                       :source (rest source)}))
+      :else (do 
+              (print-board (add-to-board board unit'))
+              {:board board
+               :unit unit'
+               :task task
+               :source source}))))
+
+(defn run-commands [task commands seed]
+  (let [source (create-source task seed)] 
+    (reduce run-one-command {:board (create-board task)
+                             :source (rest source)
+                             :task task
+                             :unit (spawn-unit task (first source))} commands)))
 
 (comment
   (def problems (load-problems))
@@ -173,4 +235,8 @@ Returns shifted unit"
     (dorun(map-indexed (fn [i u] 
                        (println "Unit" i ":")
                        (print-unit u)) (map unit->matrix (:units task)))))
+  
+  (let [task (rand-nth problems)
+        task (first problems)] 
+    (run-commands task (repeatedly 20 #(rand-nth [south-east south-west ])) (first (:sourceSeeds task))))
   )
