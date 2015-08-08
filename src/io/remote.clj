d70dfe82fecafa22fd1889a5c1253546c378c99a
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2015-08-08 14:21:33 +0200

    buggy rotation

--- src/io/remote.clj
+++ src/io/remote.clj
@@ -3,6 +3,9 @@
             [clj-http.client :as http]
             [clojure.java.io :as io]))
 
+(defn- with-index [vs]
+  (map-indexed vector vs))
+
 (def token "C/1d7grJDHHZEBserpvNAckXRf2+LGVtibuDE8ukD4k=")
 (defn load-problems []
   (map (comp json/read-json slurp) (filter #(.isFile %) (file-seq (io/as-file "resources")))))
@@ -30,7 +33,7 @@
             0)))))
 
 (defn print-board [board]
-  (doseq [[i row] (map vector (range) board) :let [odd? (= 1 (mod i 2))]]
+  (doseq [[i row] (with-index board) :let [odd? (= 1 (mod i 2))]]
     (when odd? (print "  ")) ; two space because my font seems to be proportional :(
     (doseq [cell row]
       (print ([\u2B21 \u2B22] cell) ""))
@@ -38,25 +41,25 @@
   (println))
 
 ;;;;;;;;;;;;;;;; units ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defrecord Unit [coords pivot])
+(defrecord Unit [coords pivot offsets])
 
-(defn unit->matrix [{:keys [members pivot]}]
-  (let [max-x (apply max (:x pivot) (map :x members))
-        max-y (apply max (:y pivot) (map :y members))
-        unit (for [y (range (inc max-y))]
-               (for [x (range (inc max-x))
+(defn create-unit [{:keys [members pivot]}]
+  (let [[min-x max-x] (apply (juxt min max) (map :x members))
+        [min-y max-y] (apply (juxt min max) (map :y members))
+        unit (for [y (range min-y (inc max-y))]
+               (for [x (range min-x (inc max-x))
                      :let [coord {:x x :y y}]]
                  (if (some #(= % coord) members)
                    1
                    0)))]    
-    (->Unit (mapv vec unit) [(:y pivot) (:x pivot)])))
+    (->Unit (mapv vec unit) [(:y pivot) (:x pivot)] [min-y min-x])))
 
 
 (defn print-unit [{:keys [coords pivot]}] 
-  (doseq [[i row] (map vector (range) coords) 
+  (doseq [[i row] (with-index coords) 
           :let [odd? (= 1 (mod i 2))]]
     (when odd? (print "  ")) ; two space because my font seems to be proportional :(
-    (doseq [[j cell] (map vector (range) row)
+    (doseq [[j cell] (with-index row)
             :let [pivot? (= pivot [i j])
                   chars (if pivot? [\u25ef \u25cf] [\u2B21 \u2B22])]]
       (print (chars cell) ""))
@@ -68,17 +71,27 @@
   "Convert local coordinates into board coordinates
 so the unit is centered with its top on the top row of the board.
 Returns shifted unit"
-  [board {:keys [coords pivot]}]
+  [board {:keys [coords pivot] :as unit}]
   (let [board-width (count (first board))
         unit-top-row (first coords)
         unit-width (count unit-top-row)
         offset-left (count (take-while zero? unit-top-row))
         offset-right (count (take-while zero? (reverse unit-top-row)))
         unit-top-width (- unit-width offset-left offset-right)
-        offset (- (int (/ (- board-width unit-top-width) 2)) offset-left)
-        offset-v (vec (repeat offset 0))]
-    (->Unit (mapv (partial into offset-v) coords) 
-      (update pivot 1 + offset))))
+        offset (- (int (/ (- board-width unit-top-width) 2)) offset-left)]
+    (assoc unit :offsets [0 offset])))
+
+(defn add-to-board [board {coords :coords [off-row off-col] :offsets}]
+  (reduce (fn [board [row-idx unit-row]]
+            (let [unit-row (concat (repeat off-col 0) unit-row)]
+              (update board (+ row-idx off-row) #(into (mapv + unit-row %)
+                                                       (drop (count unit-row) %))))) 
+          board (with-index coords)))
+
+(defn overlaps-anything? 
+  "If the unit would get added to the board, would any cells overlap?"
+  [board unit]
+  (every? #{0 1} (apply concat (add-to-board board unit))))
 
 ;;;;;;;;;;;;;;; coordinates ;;;;;;;;;;;;;;;;;;
 (defn grid->cube [[col row]]
@@ -97,20 +110,20 @@ Returns shifted unit"
         row z]
     [col row]))
 
-(defn rotate-cw [[x y z]]
+(defn cube-rotate-cw [[x y z]]
   [(- z) 
    (- x) 
    (- y)])
 
-(defn rotate-ccw [[x y z]]
+(defn cube-rotate-ccw [[x y z]]
   [(- y) 
    (- z) 
    (- x)])
 
 (defn rotate-around [pivot cell rotator]
-  (let [pc (grid->cube pivot)
-        cc (grid->cube cell)]
-    (cube->grid-odd-row (map + pc (rotator (map - cc pc))))))
+  (let [pc (grid->cube (reverse pivot))
+        cc (grid->cube (reverse cell))]
+    (reverse (cube->grid-even-row (map + pc (rotator (map - cc pc)))))))
 
 
 ;;;;;;;;;;;;;;; commands;;;;;;;;;;;;;;;;;;;;;;
@@ -122,14 +135,38 @@ Returns shifted unit"
 (def CW (vec "dqrvz1"))
 (def CCW (vec "kstuwx"))
 
+(defn east [unit]
+  (update-in unit [:offsets 1] inc))
 
+(defn west [unit]
+  (update-in unit [:offsets 1] dec))
 
+(defn south-east [unit]
+  (update-in (east unit) [:offsets 0] inc))
 
+(defn south-west [unit]
+  (update-in (west unit) [:offsets 0] inc))
+
+(defn- matrix-to-members [coords]
+  (for [[i row] (with-index coords)
+        [j col] (with-index row)
+        :when (= 1 col)]
+    {:x j :y i}))
+
+(defn rotate-cw [{:keys [coords pivot offsets]}]
+  (let [pivot-cube (grid->cube (map + pivot offsets))
+        members (for [{:keys [x y]} (matrix-to-members coords)
+                      :let [[x y] (rotate-around pivot [y x] cube-rotate-cw)]] 
+                  {:x x :y y})
+        [p-y p-x] pivot]
+    (update (create-unit {:members members :pivot {:x p-x :y p-y}})
+            :offsets #(mapv + offsets %))))
 
 (comment
   (def problems (load-problems))
   (def task (nth problems 2))
   (def b (board task))
+  (def units (mapv create-unit (:units task)))
   (doseq [[task-idx task] (map vector (range) problems)]
     (println "Task #" task-idx)
     (println "--------------")
---
+++ test/icfp2015_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns icfp2015-clj.core-test
+  (:require [clojure.test :refer :all]
+            [icfp2015-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
